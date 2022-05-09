802384293ba2e7c7282e7fd2f678c3571f4da3aa
Greg <85593712+csmajorbtw@users.noreply.github.com>
2022-05-09 18:35:33 -0400

    Add files via upload

---
+++ GAMEINFO/Music/Forest/startmusic.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+TITLE MUSIC
+
+:REPLAY
+C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\Forest\cmdmp3 "C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\Forest\Forest.mp3" > nul
+
+goto REPLAY
