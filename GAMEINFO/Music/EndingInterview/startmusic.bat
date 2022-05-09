cf55b90091d7301f804f055413c892bfecd9295a
Greg <85593712+csmajorbtw@users.noreply.github.com>
2022-05-09 18:35:12 -0400

    Add files via upload

---
+++ GAMEINFO/Music/EndingInterview/startmusic.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+TITLE MUSIC
+
+:REPLAY
+C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\EndingInterview\cmdmp3 "C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\EndingInterview\EndingInterview.wav" > nul
+
+goto REPLAY
