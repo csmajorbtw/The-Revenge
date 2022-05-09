1386e7c98c4075ae2d3f31f9a452faeb497a2deb
Greg <85593712+csmajorbtw@users.noreply.github.com>
2022-05-09 18:35:59 -0400

    Add files via upload

---
+++ GAMEINFO/Music/Title/startmusic.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+TITLE MUSIC
+
+:REPLAY
+C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\Title\cmdmp3 "C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\Title\Title.mp3" > nul
+
+goto REPLAY
---
+++ GAMEINFO/Music/TownScene/startmusic.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+TITLE MUSIC
+
+:REPLAY
+C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\TownScene\cmdmp3 "C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\TownScene\TownScene.wav" > nul
+
+goto REPLAY
