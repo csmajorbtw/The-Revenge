5903055348088acd4b12a4f7449b06f6421bbbc8
Greg <85593712+csmajorbtw@users.noreply.github.com>
2022-05-09 18:43:07 -0400

    Add files via upload

---
+++ GAMEINFO/Music/Goodbye/startmusic.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+TITLE MUSIC
+
+:REPLAY
+C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\Goodbye\cmdmp3 "C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\Goodbye\Goodbye.wav" > nul
+
+goto REPLAY
