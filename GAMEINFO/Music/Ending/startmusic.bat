a22641dfd530783b51bb7e955315235feadf6f02
Greg <85593712+csmajorbtw@users.noreply.github.com>
2022-05-09 18:34:57 -0400

    Add files via upload

---
+++ GAMEINFO/Music/Ending/startmusic.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+TITLE MUSIC
+
+:REPLAY
+C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\Ending\cmdmp3 "C:\Users\%USERNAME%\Desktop\TheRevenge\GAMEINFO\Music\Ending\Ending.mp3" > nul
+
+goto REPLAY
